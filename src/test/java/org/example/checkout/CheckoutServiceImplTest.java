c752e7af75fc40c2b54b11246276423cd020b7c2
marcin <marcin.gasior@gmail.com>
2023-03-06 15:24:23 +0100

    adding promocodes

--- src/test/java/org/example/checkout/CheckoutServiceImplTest.java
+++ src/test/java/org/example/checkout/CheckoutServiceImplTest.java
@@ -27,6 +27,21 @@ class CheckoutServiceImplTest {
         assertEquals(paymentForEmptyCart, total);
     }
 
+    @Test
+    void shouldCalculateCartOfDistinctItems(){
+        Cart cart = new Cart(List.of('A', 'B', 'C', 'D'));
+        var total = checkoutService.calculateTotal(cart);
+        int paymentForCart = 115;
+        assertEquals(paymentForCart, total);
+    }
+
+    @Test
+    void shouldAddDiscountForMultipleItems(){
+        Cart cart = new Cart(List.of('A', 'A', 'A', 'A'));
+        var total = checkoutService.calculateTotal(cart);
+        int paymentForCart = 180;
+        assertEquals(paymentForCart, total);
+    }
 
 
 }
